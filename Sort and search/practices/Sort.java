932a672887868b4afc72198f5dc6134998a2c5e5
Quân Nguyễn <quanlh3052004@gmail.com>
2026-09-12 16:22:18 +0700

    sort impl

--- Sort and search/practices/Sort.java
+++ Sort and search/practices/Sort.java
@@ -59,30 +59,97 @@ class Sort {
     }
     
     public static void merge(int[] arr, int l, int mid, int r) {
-        int[] tmp = new int[l-r+1];
+        int[] tmp = new int[r - l + 1];
 
         int i = l;
         int j = mid + 1;
         int k = 0;
 
         while (i <= mid && j <= r) {
-            if (arr[i] < arr[j]) {
+            if (arr[i] <= arr[j]) {
                 tmp[k++] = arr[i++];
-            }else {
+            } else {
                 tmp[k++] = arr[j++];
             }
         }
-         
+
+        while (i <= mid) {
+            tmp[k++] = arr[i++];
+        }
+        while (j <= r) {
+            tmp[k++] = arr[j++];
+        }
+
+        for (int h = 0; h < tmp.length; h++) {
+            arr[l + h] = tmp[h];
+        }
     }
 
-    public static void mergeSort(int[] arr, int l , int r) {
-        if (l > r) return;
-        int mid = l + (r-l)/2;
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
 
         mergeSort(arr, l, mid);
         mergeSort(arr, mid + 1, r);
 
         merge(arr, l, mid, r);
+    }
+
+    public static void quickSort (int[] arr, int l , int r) {
+        if (l >= r) return;
+
+        int pivot = lomutoPartition(arr, l, r);
+
+        quickSort(arr, l, pivot-1);
+        quickSort(arr, pivot+1, r);
+    }
+
+    public static int lomutoPartition(int[] arr, int l, int r) {
+        int i = l-1;
+        
+        for (int j = l; j < r; j++) {
+            if (arr[j] < arr[r]) {
+                i++;
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+        
+        int tmp = arr[i+1];
+        arr[i+1] = arr[r];
+        arr[r] = tmp;
+
+        return i+1;
+    }
+
+
+    public static int hoarePartition(int[] arr, int l, int r) {
+        
+        int pivot = arr[l + (r-l)/2];
+        int i = l - 1;
+        int j = r + 1;
+        while (true) {
+            do {
+                i++;
+            } while (arr[i] < pivot);
+
+
+            do {
+                j--;
+            } while (arr[j] > pivot);
+
+            if (i > j) break;
+
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+
+        return j;
     }
 
 
@@ -91,7 +158,8 @@ class Sort {
         int[] arr = {1,3,2,6,4,5};
         // bubbleSort(arr);
         // selectionSort(arr);
-        insertionSort(arr);
+        // insertionSort(arr);
+        quickSort(arr, 0, arr.length - 1);
 
         for (int x : arr) {
             System.out.println(x);
